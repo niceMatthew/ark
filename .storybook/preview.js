08d18227bd009e44e0724684d4ccdd268c7e34c8
jinxin <jinxinv@126.com>
2020-09-09 09:14:00 +0800

    修复storybook中没有引用storybook的问题

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 import { withInfo } from '@storybook/addon-info'
 import "../src/styles/index.scss"
+
+library.add(fas)
+
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
